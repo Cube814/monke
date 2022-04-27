d2fa2a1441085d0bcb39cec8f0e09dc5c1095e61
Cuba <Lilcuba4@icloud.com>
2022-04-27 09:45:46 -0700

    boobs

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      message: "Cubas sucks",
+      text: "Sahil's dope",
+      text2: "Where da hoes",
+      h1OG: "font-white",
+    }
+  }
+ render() {
+  return (
+    <div>
+      <div>
+      <h1 className={this.state.h1OG}>{this.state.message}</h1>
+      <button onClick={() => {
+        this.setState({
+          h1OG: "font-white-moved",
+          message: "We love Cuba",
+        })
+      }}>Click to be nicer</button>
+      </div>
+      <div>
+        <h2> {this.state.text}</h2>
+        <button onClick={
+          () => {
+            this.setState(
+              {
+                text: "Boobs"
+              }
+            )
+            console.log("balls")
+          }
+        }>Sike</button>
+      </div>
+      <div>
+        <h3>
+          {this.state.text2}</h3>
+          <button onClick={
+            () => {
+              this.setState(
+                {
+                  text2: "poggers"
+                }
+              )
+            }
+          }>touch this</button>
+          </div>
+    </div>
+    
+  )
+
+ }
+    
+}
+
+export default App
